ba109abf5711f165587641ec6079528a878eab72
tania77 <ainatrg@gmail.com>
2016-10-13 09:59:35 +0000

    cosas

--- index.js
+++ index.js
@@ -1,6 +1,32 @@
-var http = require('http');
-var server = http.createServer(function(req, res) {
-    res.writeHead(200, {'content-type': 'text/html'});
-    res.end('<h1>Hello World</h1');
+"use strict";
+
+var express = require('express');
+var app = express();
+var path = require('path');
+var expressLayouts = require('express-ejs-layouts');
+
+
+// var users = require('./routes/users');
+
+
+
+app.set('port', (process.env.PORT || 8080));
+
+
+app.use(expressLayouts);
+
+//app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('gh-pages'));
+
+
+app.get('/', (request, response) => {
+  response.send('index');  
 });
-server.listen(3000);
+
+app.listen(app.get('port'), () => {
+  console.log(`Puerto:${app.get('port')}`);
+});
+
+module.exports = app;
+
+
