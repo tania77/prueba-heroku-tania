7f343242aa7e90229508a102a0006718b57936ff
tania77 <ainatrg@gmail.com>
2016-10-13 10:12:42 +0000

    añadido start

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(express.static('gh-pages'));
 
 
 app.get('/', (request, response) => {
-  response.send('index');  
+  response.render('gh-pages/index.html');  
 });
 
 app.listen(app.get('port'), () => {
